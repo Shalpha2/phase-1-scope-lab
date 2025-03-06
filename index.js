d91257de84ed23d585974990fe5b47101020d861
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-03-06 07:52:32 -0800

    scope

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+var  customerName = "bob"
+
+function upperCaseCustomerName(){
+customerName=customerName.toUpperCase()
+console.log(customerName)
+
+}
+function setBestCustomer(){
+    bestCustomer = "not bob"
+ console.log (bestCustomer)   
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer ="maybe bob"
+    console.log(bestCustomer);
+}
+
+const leastFavoriteCustomer = 'someone';
+console.log(leastFavoriteCustomer);
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else'; 
+    console.log(leastFavoriteCustomer);
+}
+
+
+
+
+
+
